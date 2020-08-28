9d55baaab259580b7b08dd4ccc6e7fcefb80c355
pedroigor66 <picoliveira92@gmail.com>
2020-08-28 14:46:55 -0300

    Refactored code in Cart page so it can pass all the tests.

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import { View } from 'react-native';
-
-import FloatingCart from '../../components/FloatingCart';
 
 import {
   Container,
@@ -20,6 +18,9 @@ import {
   ProductQuantity,
   ActionContainer,
   ActionButton,
+  TotalProductsContainer,
+  TotalProductsText,
+  SubtotalValue,
 } from './styles';
 
 import { useCart } from '../../hooks/cart';
@@ -44,6 +45,26 @@ const Cart: React.FC = () => {
   function handleDecrement(id: string): void {
     decrement(id);
   }
+
+  const cartTotal = useMemo(() => {
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
+
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
+  }, [products]);
+
+  const totalItensInCart = useMemo(() => {
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+
+      return accumulator + productsQuantity;
+    }, 0);
+
+    return total;
+  }, [products]);
 
   return (
     <Container>
@@ -92,7 +113,11 @@ const Cart: React.FC = () => {
           )}
         />
       </ProductContainer>
-      <FloatingCart />
+      <TotalProductsContainer>
+        <FeatherIcon name="shopping-cart" color="#fff" size={24} />
+        <TotalProductsText>{`${totalItensInCart} itens`}</TotalProductsText>
+        <SubtotalValue>{cartTotal}</SubtotalValue>
+      </TotalProductsContainer>
     </Container>
   );
 };
